e4f0f8006d20271a3d8e576faa8873e6823cf374
Дмитрий <likstern@gmail.com>
2018-03-13 08:04:50 +0300

    soreted by key x and y

--- geometry.py
+++ geometry.py
@@ -1,7 +1,17 @@
 from geom2d import *
 
 l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
-l2 = sorted(l1)
+
+
+def x(p):
+    return p.x
+
+
+def y(p):
+    return p.y
+
+
+l2 = sorted(l1, key=x)
 
 print('ok')
 print(l1)
